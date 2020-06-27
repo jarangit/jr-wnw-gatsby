ae34ac9a366981ea8a80e882d70b7c37706fff13
jarangit <jaran.dch@gmail.com>
2020-06-27 15:43:20 +0700

    123465

---
+++ src/components/Layout/Menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Menu = () => {
+    return(
+        <div>
+            this menu
+        </div>
+    )
+}
+
+export default Menu
---
+++ src/components/Layout/layout.js
@@ -0,0 +1,35 @@
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import "./layout.css"
+import Menu from './Menu'
+import Footer from "./Footer"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+    <Menu/>
+      <div>
+        <main>{children}</main>
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
